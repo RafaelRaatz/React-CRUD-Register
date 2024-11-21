c344806287fb58c52152ac4703c766a5c496086c
unknown <raatzrafael@gmail.com>
2024-11-20 21:37:05 -0300

    :tada: finished project

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Container } from './styles';
 import { AiFillPlusCircle } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 export function Header({ hideButton, title }) {
+  function registerLink() {
+    const navigate = useNavigate();
+
+    navigate('/register');
+  }
+
   return (
     <Container>
       <div className="header">
@@ -12,8 +19,7 @@ export function Header({ hideButton, title }) {
         </div>
         {!hideButton && (
           <a href="/register">
-            <button className="header-button">
-              {' '}
+            <button className="header-button" onClick={registerLink}>
               <AiFillPlusCircle className="icon-button" /> Add Player
             </button>
           </a>
